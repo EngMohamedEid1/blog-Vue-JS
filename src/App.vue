79396b77c45e93ebe2ed9bc690907a192585b2bf
Eng Eid <eng.eid2030@gmail.com>
2023-02-18 21:03:23 +0200

    blog

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <website-header
+    title="my website"
+    describtion="my smart blog"
+  ></website-header>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import WebsiteHeader from "./components/WebsiteHeader.vue";
+import posts from "../posts.json";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { WebsiteHeader },
+  name: "App",
+  data() {
+    return {
+      posts,
+    };
+  },
+  provide() {
+    return {
+      posts: this.posts,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
